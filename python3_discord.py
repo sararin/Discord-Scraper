527d54b04f1c53ff6165f3b5bcc4e7c598d7b49e
KimChoJapFan <actinoid02@yahoo.com>
2018-06-22 05:22:14 -0500

    Update Python 3 Script again
    I accidentally uploaded the wrong version of the script.

--- python3_discord.py
+++ python3_discord.py
@@ -1,70 +1,62 @@
-from urllib3 import disable_warnings, exceptions
-from requests import get, post, delete, codes
-from random import randrange as rand
-from os import path, mkdir, getcwd
-from threading import Thread as th
-from json import loads as toarr
-from shutil import copyfileobj
-from sys import stdout
+import http.client, random, json, os
 
-disable_warnings(exceptions.InsecureRequestWarning)
-charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-rstring = lambda length: [charset[rand(0, len(charset)-1)] for x in range(length)]
+# Generate random characters for when there are duplicate filenames.
+charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+random_string = lambda length: [random.choice(charset) for x in range(length)]
 
+# Discord class
 class Discord:
-    def __init__(self, token, server, channel):
-        self.agent   = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.300 Chrome/56.0.2924.87 Discord/1.6.15 Safari/537.36'
-        self.channel = channel
-        self.server  = server
-        self.token   = token
-        self.query   = ''
 
-    def setQuery(self, isNSFW=True, hasLink=False, hasEmbed=False, hasFile=False, hasVideo=False, hasImage=True, hasSound=False):
-        if isNSFW: self.query   = 'include_nsfw=true'
-        if hasLink: self.query  =  '{}&has=link'.format(self.query)
-        if hasEmbed: self.query = '{}&has=embed'.format(self.query)
-        if hasFile: self.query  =  '{}&has=file'.format(self.query)
-        if hasVideo: self.query = '{}&has=video'.format(self.query)
-        if hasImage: self.query = '{}&has=image'.format(self.query)
-        if hasSound: self.query = '{}&has=sound'.format(self.query)
+    # Constructor
+    def __init__(self):
+        self.token      = '<TOKEN>'
+        self.server     = '<SERVER ID>'
+        self.channel    = '<CHANNEL ID>'
+        self.query      = ''
+        self.jsondata   = []
 
-    def grabJSON(self, page=0):
-        url, header = 'https://discordapp.com/api/v6/guilds/{}/messages/search?{}&channel_id={}&offset={}'.format(self.server, self.query, self.channel, page * 25), {'user-agent': self.agent, 'authorization': self.token}
-        jsonContent = get(url, headers=header, verify=False, allow_redirects=False)
+    # Generate query
+    def new_query(self, is_nsfw=True, has_image=True, has_video=True):
+        self.query = 'include_nsfw=true' if is_nsfw else self.query
+        self.query = '{}&has=image'.format(self.query) if has_image else self.query
+        self.query = '{}&has=video'.format(self.query) if has_video else self.query
 
-        if jsonContent.status_code == codes.ok: return toarr(jsonContent.text)
-        else: stdout.write('Failed to grab JSON data from \'{}\'.\n'.format(url))
+    # Grab JSON from the Discord search function
+    def grab_json(self, pages=1):
+        for i in range(0, pages):
+            conn = http.client.HTTPSConnection('discordapp.com', 443)
+            conn.request('GET', '/api/v6/guilds/{}/messages/search?{}&channel_id={}&offset={}'.format(self.server, self.query, self.channel, i*25), headers={'authorization': self.token})
+            self.jsondata.append(conn.getresponse().read().decode('utf-8'))
 
-    def saveFile(self, fileURL, dataPath):
-        rawFileData = get(fileURL, stream=True)
-        if rawFileData.status_code == codes.ok:
-            rawFileName = path.join(dataPath, fileURL.split('/')[-1])
-            if path.exists(rawFileName): rawFileName = path.join(dataPath, '{}-{}'.format(''.join(rstring(8)), fileURL.split('/')[-1]))
-            with open(rawFileName, 'wb') as fStream: copyfileobj(rawFileData.raw, fStream)
-        del rawFileData
+    # Sort through the JSON data and pick out the images and then download them
+    def grab_files(self):
 
+        # Determine if download path exists; otherwise create it
+        download_path = os.path.join(os.getcwd(), 'discord_scrapes')
+        if not os.path.exists(download_path): os.mkdir(download_path)
+
+        # Time to traverse through the JSON data
+        for json_data in self.jsondata:
+            json_data = json.loads(json_data)
+
+            # Time to traverse through the messages
+            for message in json_data['messages']:
+                try:
+                    file_url, file_name, file_size = message[0]['attachments'][0]['url'], os.path.join(download_path, message[0]['attachments'][0]['filename']), message[0]['attachments'][0]['size']
+                    if os.path.exists(file_name): file_name = os.path.join(download_path, '{}_{}'.format(''.join(random_string(8)), message[0]['attachments'][0]['filename']))
+                    
+                    # Download the image file
+                    conn = http.client.HTTPSConnection('cdn.discordapp.com', 443)
+                    conn.request('GET', '/{}'.format('/'.join(file_url.split('/')[3::])))
+                    with open(file_name, 'wb') as fstream:
+                        fstream.write(conn.getresponse().read(file_size))
+                except IndexError:
+                    continue
+
+# Connect to the class and carry out the necessary functions
 if __name__ == '__main__':
-    discord = Discord('<TOKEN>', 0, 0)
-    discord.setQuery(True, True, True, True, True, True)
-
-    numPages, folderName, dataArray = int(input('Pages to scrape: ')), 'discord_scrapes', []
-    dataPath = path.join(getcwd(), folderName)
-    if not path.exists(dataPath): mkdir(dataPath)
-
-    for i in range(numPages):
-        jsonData = discord.grabJSON(i)
-
-        for messages in jsonData['messages']:
-            for x in range(len(messages)):
-                for attachments in messages[x]['attachments']: dataArray.append(attachments['url'])
-
-        dataArray, threads = list(set(dataArray)), []
-        for data in dataArray:
-            t = th(target=discord.saveFile, args=(data, dataPath, ))
-            threads.append(t)
-
-        for thread in threads:
-            thread.start()
-            thread.join()
-
-        del threads[:]
+    discord = Discord()
+    discord.new_query()
+    discord.grab_json(input('Number of pages to grab: '))
+    discord.grab_files()
+    

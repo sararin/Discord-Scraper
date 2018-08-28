6780549752ca2d05191ff171b51b746440116293
KimChoJapFan <actinoid02@yahoo.com>
2018-08-28 03:13:51 -0500

    Major Experimental Change
    This should help keep people from accidentally leaking their Discord Tokens when they share their scripts before removing those tokens from them.
    
    So with this change comes an example config.json file and this opens the door for scraping from multiple servers and channels and I'll clean up things in the future releases.
    
    I also have plans on just bypassing the whole offset method and hopefully make things faster and prevent any potential duplicates that slip through the faulty filtering system.

--- python3_discord.py
+++ python3_discord.py
@@ -1,62 +1,96 @@
-import http.client, random, json, os
+import http.client, threading, random, json, html, sys, os
+charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+randomString = lambda length: ''.join([random.choice(charset) for x in range(length)])
 
-# Generate random characters for when there are duplicate filenames.
-charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-random_string = lambda length: [random.choice(charset) for x in range(length)]
+class MissingValue(Exception):
+    def __init__(self, valueName, fileName):
+        sys.stderr.write('{} is invalid in {}'.format(valueName, fileName))
 
-# Discord class
-class Discord:
+class Requests:
+    def __init__(self, headers):
+        self.headers = headers
 
-    # Constructor
-    def __init__(self):
-        self.token      = '<TOKEN>'
-        self.server     = '<SERVER ID>'
-        self.channel    = '<CHANNEL ID>'
-        self.query      = ''
-        self.jsondata   = []
-
-    # Generate query
-    def new_query(self, is_nsfw=True, has_image=True, has_video=True):
-        self.query = 'include_nsfw=true' if is_nsfw else self.query
-        self.query = '{}&has=image'.format(self.query) if has_image else self.query
-        self.query = '{}&has=video'.format(self.query) if has_video else self.query
-
-    # Grab JSON from the Discord search function
-    def grab_json(self, pages=1):
-        for i in range(0, pages):
-            conn = http.client.HTTPSConnection('discordapp.com', 443)
-            conn.request('GET', '/api/v6/guilds/{}/messages/search?{}&channel_id={}&offset={}'.format(self.server, self.query, self.channel, i*25), headers={'authorization': self.token})
-            self.jsondata.append(conn.getresponse().read().decode('utf-8'))
-
-    # Sort through the JSON data and pick out the images and then download them
-    def grab_files(self):
-
-        # Determine if download path exists; otherwise create it
-        download_path = os.path.join(os.getcwd(), 'discord_scrapes')
-        if not os.path.exists(download_path): os.mkdir(download_path)
-
-        # Time to traverse through the JSON data
-        for json_data in self.jsondata:
-            json_data = json.loads(json_data)
-
-            # Time to traverse through the messages
-            for message in json_data['messages']:
-                try:
-                    file_url, file_name, file_size = message[0]['attachments'][0]['url'], os.path.join(download_path, message[0]['attachments'][0]['filename']), message[0]['attachments'][0]['size']
-                    if os.path.exists(file_name): file_name = os.path.join(download_path, '{}_{}'.format(''.join(random_string(8)), message[0]['attachments'][0]['filename']))
-                    
-                    # Download the image file
-                    conn = http.client.HTTPSConnection('cdn.discordapp.com', 443)
-                    conn.request('GET', '/{}'.format('/'.join(file_url.split('/')[3::])))
-                    with open(file_name, 'wb') as fstream:
-                        fstream.write(conn.getresponse().read(file_size))
-                except IndexError:
-                    continue
-
-# Connect to the class and carry out the necessary functions
-if __name__ == '__main__':
-    discord = Discord()
-    discord.new_query()
-    discord.grab_json(input('Number of pages to grab: '))
-    discord.grab_files()
+    def splitUrl(self, url):
+        scheme, blank, self.domain = url.split('/')[0:3]
+        query = '/'.join(url.split('/')[3::])
+        
+        self.port = 80 if scheme == 'http:' else 443
+        self.query = '/{}'.format(query)
     
+    def get(self, url):
+        self.splitUrl(url)
+        
+        conn = http.client.HTTPConnection(self.domain, 80) if self.port == 80 else http.client.HTTPSConnection(self.domain, 443)
+        conn.request('GET', self.query, headers=self.headers)
+
+        resp = conn.getresponse()
+        data = resp.read()
+
+        self.status, self.reason, self.raw, self.text = resp.status, resp.reason, data, html.unescape(data.decode('iso-8859-1'))
+
+class DiscordSpider:
+    def __init__(self, config='config.json'):
+        fileName = os.path.join(os.getcwd(), config)
+        
+        try:
+            if not os.path.exists(fileName): raise FileNotFoundError
+            threads = []
+            
+            with open(fileName, 'r') as configFile:
+                configJSON = json.loads(configFile.read())
+
+            if not configJSON['token']: raise MissingValue('Discord Authorization Token', fileName)
+            if not configJSON['query']: raise MissingValue('Discord Search Query', fileName)
+            if len(configJSON['servers']) == 0: sys.stderr.write('No Servers to Crawl')
+
+            self.httpHeaders = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.300 Chrome/56.0.2924.87 Discord/1.6.15 Safari/537.36',
+                'authorization': configJSON['token']
+            }
+
+            for serverId, v in configJSON['servers'].items():
+                for channelId, folderName in configJSON['servers'][serverId].items():
+                    if not os.path.exists(os.path.join(os.getcwd(), folderName)): os.mkdir(os.path.join(os.getcwd(), folderName))
+                    for offset in range(0, 200):
+                        thread = threading.Thread(target=self.grabJSON, args=(folderName, serverId, channelId, configJSON['query'], offset, ))
+                        threads.append(thread)
+
+            for thread in threads:
+                thread.start()
+                thread.join()
+
+            del threads[:]
+
+        except ValueError:
+            sys.stderr.write('{} is not a valid JSON file.\n'.format(fileName))
+            
+        except FileNotFoundError:
+            sys.stderr.write('{} can not be found.\n'.format(fileName))
+            
+        except IOError:
+            sys.stderr.write('Failed to read file: {}\n'.format(fileName))
+
+        except MissingValue: pass
+
+    def grabJSON(self, folderName, serverId, channelId, query, offset):
+        req, res = Requests(self.httpHeaders), Requests({})
+        req.get('https://discordapp.com/api/v6/guilds/{}/messages/search?{}&channel_id={}&offset={}'.format(serverId, query, channelId, offset * 25))
+        jsonData = json.loads(req.text)
+
+        imagelinks = []
+        for messages in jsonData['messages']:
+            for x in range(len(messages)):
+                for attachments in messages[x]['attachments']:
+                    imagelinks.append(attachments['url'])
+
+        imagelinks = list(set(imagelinks))
+        for images in imagelinks:
+            res.get(images)
+
+            if res.status == 200:
+                fileName = os.path.join(os.getcwd(), folderName, '{}_{}'.format(randomString(8), images.split('/')[-1]))
+                with open(fileName, 'wb') as rawFile:
+                    rawFile.write(res.raw)
+
+if __name__ == '__main__':
+    ds = DiscordSpider()
